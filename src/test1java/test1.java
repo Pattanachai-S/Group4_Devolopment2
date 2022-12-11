51bc08aa8dfa67ca64fd67342eee9a9e981b73c1
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2022-12-11 14:00:04 +0700

    test loop

--- src/test1java/test1.java
+++ src/test1java/test1.java
@@ -3,7 +3,14 @@ package test1java;
 public class test1 {
 	  public static void main(String[] args) {
 		    System.out.println("Hello World");
+		    test_loop();
 		  }
 	  
-	  
+	  public static void test_loop() {
+		  int i = 0;
+		  while (i < 5) {
+		    System.out.println(i);
+		    i++;
+		  }
+	  }
 }
