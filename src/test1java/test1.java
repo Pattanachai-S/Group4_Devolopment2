54476cf9b878919cdfb03b4f6bf08fe8e78f9fea
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2022-12-11 16:00:20 +0700

    test for loop

--- src/test1java/test1.java
+++ src/test1java/test1.java
@@ -2,15 +2,26 @@ package test1java;
 
 public class test1 {
 	  public static void main(String[] args) {
-		    System.out.println("Hello World");
-		    test_loop();
+		    //System.out.println("Hello World");
+		    //test_loop();
+		    test_for();
 		  }
 	  
 	  public static void test_loop() {
+		  System.out.println("test loop");
 		  int i = 0;
 		  while (i < 5) {
+			System.out.print("i = ");
 		    System.out.println(i);
 		    i++;
 		  }
+	  }
+	  
+	  public static void test_for() {
+		  System.out.println("test For loop");
+		  for (int i = 0; i < 5; i++) {
+			  System.out.print("i = ");
+			  System.out.println(i);
+			}
 	  }
 }
