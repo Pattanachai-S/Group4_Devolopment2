b6d4a0d584b00d09e05c272dc166c3ea5820b239
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2022-12-27 03:48:02 +0700

    create mouse listener

--- src/OX_but_MVC/Controller.java
+++ src/OX_but_MVC/Controller.java
@@ -1,9 +1,12 @@
 package OX_but_MVC;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
 
 // Run project on this class
 public class Controller implements ActionListener{
+	MouseListener mouse_listener = new MouseListener();
 	Model table;
 	View GUI;
 	
@@ -32,19 +35,41 @@ public class Controller implements ActionListener{
 		GUI.update();  // Update everything to GUI
 	}
 	
-	// Even for reset button
+	/** event when mouse clicked*/
+	public void mouseClicked(MouseEvent e) 
+    {
+        // Finds the location of the mouse
+        PointerInfo a = MouseInfo.getPointerInfo();
+        Point b = a.getLocation();
+
+        // Gets the x -> and y co-ordinates
+        int x = (int) b.getX();
+        int y = (int) b.getY();
+        System.out.println("Mouse x: " + x);
+        System.out.println("Mouse y: " + y);
+
+//        // Determines which tile the click occured on
+//        int xTile = x/tileSize;
+//        int yTile = y/tileSize;
+//
+//        System.out.println("X Tile: " + xTile);
+//        System.out.println("Y Tile: " + yTile);
+
+    }
+	
+	// Event for reset button
 	public void even_reset(ActionEvent e) {
 		table.reset_table();
 		GUI.update();	
 	}
 	
-	// Even for save button
+	// Event for save button
 	public void even_save(ActionEvent e) {
 		table.save_file_on_form();
 		GUI.update();
 	}
 	
-	// Even for load button
+	// Event for load button
 	public void even_load(ActionEvent e) {
 		table.load_file_on_form();
 		GUI.update();
@@ -58,5 +83,18 @@ public class Controller implements ActionListener{
 		
 	}
 	
-	
+	private class MouseListener extends MouseAdapter 
+	{
+		PointerInfo info;
+		Point location ;
+	    public void mouseClicked(MouseEvent e) 
+	    {
+	        // Finds the location of the mouse
+	    	info = MouseInfo.getPointerInfo();
+	    	location = info.getLocation();
+	        int x = (int) location.getX();
+	        int y = (int) location.getY();
+	        System.out.println("Mouse : " + x+","+ y);
+	    }
+	}
 }
