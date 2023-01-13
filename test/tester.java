c14c038c30b93c08ba194575272ab01583bed329
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2023-01-13 14:14:05 +0700

    add junit

---
+++ test/tester.java
@@ -0,0 +1,14 @@
+
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class tester {
+	   @Test
+	   public void testSetup() {
+	      String str= "I am done with Junit setup";
+	      assertEquals("I am done with Junit setup",str);
+	   }
+} 
