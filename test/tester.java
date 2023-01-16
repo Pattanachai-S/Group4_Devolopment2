101da1496071c926abfc1ec2f37d7b5513494e20
Pattanachai-S <69448138+Pattanachai-S@users.noreply.github.com>
2023-01-16 12:00:58 +0700

    add check draw

--- test/tester.java
+++ test/tester.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertEquals;
 
 
 public class tester {
+	
 	   @Test
 	   public void testSetup() {
 	      String str= "I am done with Junit setup";
@@ -22,5 +23,20 @@ public class tester {
 	      model.fill_table(0, 1);  // X
 	      model.fill_table(1, 0);  // O
 	      assertEquals(1,model.get_winner());  // Player 1 win
+		  }
+	   
+	   @Test
+	   public void test_draw() {
+		  // test model can play and win
+		  System.out.println("test draw game");
+	      Model model = new Model();
+	      model.change_table_size(3);
+	      model.fill_table(1, 1);  // O in middle
+	      for (int i=0;i<3;i++) {
+	    	  for (int j=0;j<3;j++) {
+	    		  model.fill_table(i, j);
+	    	  }	    	  
+	      }
+	      assertEquals(3,model.get_winner());  // Draw
 		  }
 } 
